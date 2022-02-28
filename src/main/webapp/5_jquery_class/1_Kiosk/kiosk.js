47fe220ebfe5aa9c3ea0b7302779ed9a5d1e6b00
413hjh413 <413hjh413@google.com>
2022-02-28 18:31:44 +0900

    2022-02-28

---
+++ src/main/webapp/5_jquery_class/1_Kiosk/kiosk.js
@@ -0,0 +1,16 @@
+$(function (){//jquery start
+    let price1 = $('#price1')
+    let price2 = $('#price2')
+    let price3 = $('#price3')
+    let price4 = $('#price4')
+    let price5 = $('#price5')
+    let price6 = $('#price6')
+    let price7 = $('#price7')
+    let price8 = $('#price8')
+    let price9 = $('#price9')
+    let price10 = $('#price10')
+    let price11 = $('#price11')
+    let price12 = $('#price12')
+
+
+});//jquery end
